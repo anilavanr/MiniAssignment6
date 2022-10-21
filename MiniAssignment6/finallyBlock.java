65e89fdea35e4a128b8b7f010a86c8b82734a361
anilavanr <anilavanr@deloitte.com>
2022-10-21 11:00:16 +0530

    done

---
+++ MiniAssignment6/finallyBlock.java
@@ -0,0 +1,17 @@
+public class finallyBlock{
+    public static void main(String args[]){
+        try{
+            int data= 25/5;
+            System.out.println(data);
+        }
+        catch(NullPointerException e){
+            System.out.println(e);
+        }
+
+        finally{
+            System.out.println("finally block is executed");
+        }
+
+        System.out.println("Thank you");
+    }
+}
